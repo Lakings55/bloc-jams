f45d18015e469a4ee4d865b24773b0d28cd8090a
LaKings55 <tciuff55@gmail.com>
2017-04-05 22:11:37 -0700

    Assignment 8 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -9,6 +9,8 @@ var pointsArray = document.getElementsByClassName('point');
          points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
      }
      
+     
+     
      for (var i = 0; i < points.length; i++) {
             revealPoint(i);
      }
